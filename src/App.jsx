37dc45253eae7e7ee4ee901c94827479258e1fa7
Porth Wenny <abayla88@gmail.com>
2024-05-19 04:09:19 +0800

    🎨 Apply formatting for readability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react';
-import { Stack, Box } from '@mui/material';
-import './styles.css'
+import { useState } from "react";
+import { Stack, Box } from "@mui/material";
+import "./styles.css";
 
 function App() {
-
   const [isVisible, setIsVisible] = useState(false);
 
   const toggleVisibility = () => {
@@ -11,83 +10,111 @@ function App() {
   };
 
   function endTurn() {
-    alert('Button clicked.');
+    alert("Button clicked.");
   }
 
   function changeCookieState() {
-    alert('Cookie state changed.');
+    alert("Cookie state changed.");
   }
 
   function Cookie() {
-    return (
-      <img src="cookie-normal.png" alt="cookie" className="cookie-img"/>
-    )
+    return <img src="cookie-normal.png" alt="cookie" className="cookie-img" />;
   }
 
   return (
     <>
       <div className="header">
-        <div className="title">
-        NIM GAME
-        </div>
+        <div className="title">NIM GAME</div>
 
         <div className="subtitle">
-        a simple game <br></br> of math and strategy
+          a simple game <br></br> of math and strategy
         </div>
       </div>
 
       <div className="game-box">
         <div className="cookie-stacks">
           <div className="slot-1" onClick={changeCookieState}>
-          <Stack 
-            justifyContent="center"
-            alignItems="flex-start"
-            spacing={-23}
-          >
-            <Box><Cookie /></Box>
-          </Stack>
+            <Stack
+              justifyContent="center"
+              alignItems="flex-start"
+              spacing={-23}
+            >
+              <Box>
+                <Cookie />
+              </Box>
+            </Stack>
           </div>
 
           <div className="slot-2" onClick={changeCookieState}>
-          <Stack 
-            justifyContent="center"
-            alignItems="flex-start"
-            spacing={-23}
-          >
-            <Box style={{ zIndex: 3 }}><Cookie /></Box>
-            <Box style={{ zIndex: 2 }}><Cookie /></Box>
-            <Box style={{ zIndex: 1 }}><Cookie /></Box>
-          </Stack>
+            <Stack
+              justifyContent="center"
+              alignItems="flex-start"
+              spacing={-23}
+            >
+              <Box style={{ zIndex: 3 }}>
+                <Cookie />
+              </Box>
+              <Box style={{ zIndex: 2 }}>
+                <Cookie />
+              </Box>
+              <Box style={{ zIndex: 1 }}>
+                <Cookie />
+              </Box>
+            </Stack>
           </div>
 
           <div className="slot-3" onClick={changeCookieState}>
-          <Stack 
-            justifyContent="center"
-            alignItems="flex-start"
-            spacing={-23}
-          >
-            <Box style={{ zIndex: 5 }}><Cookie /></Box>
-            <Box style={{ zIndex: 4 }}><Cookie /></Box>
-            <Box style={{ zIndex: 3 }}><Cookie /></Box>
-            <Box style={{ zIndex: 2 }}><Cookie /></Box>
-            <Box style={{ zIndex: 1 }}><Cookie /></Box>
-          </Stack>
+            <Stack
+              justifyContent="center"
+              alignItems="flex-start"
+              spacing={-23}
+            >
+              <Box style={{ zIndex: 5 }}>
+                <Cookie />
+              </Box>
+              <Box style={{ zIndex: 4 }}>
+                <Cookie />
+              </Box>
+              <Box style={{ zIndex: 3 }}>
+                <Cookie />
+              </Box>
+              <Box style={{ zIndex: 2 }}>
+                <Cookie />
+              </Box>
+              <Box style={{ zIndex: 1 }}>
+                <Cookie />
+              </Box>
+            </Stack>
           </div>
 
           <div className="slot-4" onClick={changeCookieState}>
-          <Stack 
-            justifyContent="center"
-            alignItems="flex-start"
-            spacing={-23}
-          >
-            <Box style={{ zIndex: 7 }}><Cookie /></Box>
-            <Box style={{ zIndex: 6 }}><Cookie /></Box>
-            <Box style={{ zIndex: 5 }}><Cookie /></Box>
-            <Box style={{ zIndex: 4 }}><Cookie /></Box>
-            <Box style={{ zIndex: 3 }}><Cookie /></Box>
-            <Box style={{ zIndex: 2 }}><Cookie /></Box>
-            <Box style={{ zIndex: 1 }}><Cookie /></Box>
-          </Stack>
+            <Stack
+              justifyContent="center"
+              alignItems="flex-start"
+              spacing={-23}
+            >
+              <Box style={{ zIndex: 7 }}>
+                <Cookie />
+              </Box>
+              <Box style={{ zIndex: 6 }}>
+                <Cookie />
+              </Box>
+              <Box style={{ zIndex: 5 }}>
+                <Cookie />
+              </Box>
+              <Box style={{ zIndex: 4 }}>
+                <Cookie />
+              </Box>
+              <Box style={{ zIndex: 3 }}>
+                <Cookie />
+              </Box>
+              <Box style={{ zIndex: 2 }}>
+                <Cookie />
+              </Box>
+              <Box style={{ zIndex: 1 }}>
+                <Cookie />
+              </Box>
+            </Stack>
           </div>
         </div>
 
@@ -96,15 +123,15 @@ function App() {
         </button>
 
         <div className="show-button" onClick={toggleVisibility}>
-        {isVisible ? 'Hide Text' : 'Show Text'}
+          {isVisible ? "Hide Text" : "Show Text"}
         </div>
-      {isVisible && <p> Text Shown (placeholder for best play text.) </p>}
+        {isVisible && <p> Text Shown (placeholder for best play text.) </p>}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 
 // success message
-console.log('App.jsx loaded')
+console.log("App.jsx loaded");
